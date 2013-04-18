12e5ff4d7b853b63ee961940242c15b6a3d85a70
Atomiko466 <serega-96-anime@mail.ru>
2013-04-18 21:09:37 +0400

    Sr.Ar.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 6 чисел и нажмите ENTER");
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("m=");
+            int m = int.Parse(Console.ReadLine());
+            Console.Write("v=");
+            int v = int.Parse(Console.ReadLine());
+            Console.Write("c=");
+            int c = int.Parse(Console.ReadLine());
+            Console.Write("x=");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("z=");
+            int z = int.Parse(Console.ReadLine());
+
+
+            int[] array1 = new int[] { n, m, v, c, x, z };
+            int summ = 0;
+            for (int i = 0; i < array1.Length; i++) ;
+            int mid;
+            {
+                summ = n + m + v + c + x + z;
+            }
+            mid = summ / array1.Length;
+            Console.Write("Среднее арифметическое=", mid);
+            Console.WriteLine();
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
